c1e695ed56640e217501d5bef2ec2b43d05b34d9
namy2 <namy2@mcmaster.ca>
2024-05-01 15:54:45 +0900

    changes to data pipeline

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     ],
     extras_require={
         "tensorflow": ["tensorflow"],
-        "tensorflow with gpu": ["tensorflow-gpu"]
+        "convokit": ["convokit"],
+        "tensorflow with gpu": ["tensorflow-gpu"],
     },
 )
