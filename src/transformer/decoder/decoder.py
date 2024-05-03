f213ec5f85c67de5602a7dd342d11f251db59e50
namy2 <namy2@mcmaster.ca>
2024-05-03 20:59:54 +0900

    implemented transformer decoder model

---
+++ src/transformer/decoder/decoder.py
@@ -0,0 +1,35 @@
+"""
+Decoder
+"""
+
+import tensorflow as tf
+
+from src.transformer.encoder import positional_encoding
+from src.transformer.decoder import decoder_layer
+
+def decoder(vocab_size, num_layers, units, d_model, num_heads, dropout, name='decoder'):
+    inputs = tf.keras.Input(shape=(None, ), name='inputs')
+    encoder_outputs = tf.keras.Input(shape=(None, d_model), name='encoder_outputs')
+    look_ahead_mask = tf.keras.Input(shape=(1, None, None), name='look_ahead_mask')
+    padding_mask = tf.keras.Input(shape=(1, 1, None), name='padding_mask')
+
+    embeddings = tf.keras.layers.Embedding(vocab_size, d_model)(inputs)
+    embeddings *= tf.math.sqrt(tf.cast(d_model, tf.float32))
+    embeddings = positional_encoding.PositionalEncoding(vocab_size, d_model)(embeddings)
+
+    outputs = tf.keras.layers.Dropout(rate=dropout)(embeddings)
+
+    for i in range(num_layers):
+        outputs = decoder_layer.decoder_layer(
+            units=units,
+            d_model=d_model,
+            num_heads=num_heads,
+            dropout=dropout,
+            name='decoder_layer_{}'.format(i)
+        )(inputs=[outputs, encoder_outputs, look_ahead_mask, padding_mask])
+
+    return tf.keras.Model(
+        inputs=[inputs, encoder_outputs, look_ahead_mask, padding_mask],
+        outputs=outputs,
+        name=name
+    )
