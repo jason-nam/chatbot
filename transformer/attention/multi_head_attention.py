e50dd2781962893e0627e45485d34eeb797138cc
namy2 <namy2@mcmaster.ca>
2024-05-15 15:54:33 +0900

    implemented and troubleshoot multi head attention unit test

--- transformer/attention/multi_head_attention.py
+++ transformer/attention/multi_head_attention.py
@@ -16,14 +16,16 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         assert d_model % self.num_heads == 0
 
         self.depth = d_model // self.num_heads
+
         self.query_dense = tf.keras.layers.Dense(units=d_model)
         self.key_dense = tf.keras.layers.Dense(units=d_model)
         self.value_dense = tf.keras.layers.Dense(units=d_model)
+
         self.dense = tf.keras.layers.Dense(units=d_model)
 
     def split_heads(self, inputs, batch_size):
         re_inputs = tf.reshape(inputs, shape=(batch_size, -1, self.num_heads, self.depth))
-        return tf.transpose(re_inputs, perm=[0, 1, 2, 3])
+        return tf.transpose(re_inputs, perm=[0, 2, 1, 3])
     
     def call(self, inputs):
         query, key, value, mask = inputs['query'], inputs['key'], inputs['value'], inputs['mask']
@@ -43,7 +45,7 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
 
         # concatenation of heads
-        concat_attention = tf.reshape(scaled_attention, batch_size, -1, self.d_model) 
+        concat_attention = tf.reshape(scaled_attention, (batch_size, -1, self.d_model))
 
         # final linear layer
         output = self.dense(concat_attention)
