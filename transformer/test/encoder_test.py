c296d67ce04b468651add3c9334f938f1312aa13
namy2 <namy2@mcmaster.ca>
2024-05-16 19:59:30 +0900

    implemented encoder function unit test

--- transformer/test/encoder_test.py
+++ transformer/test/encoder_test.py
@@ -28,6 +28,7 @@ class TestEncoder(unittest.TestCase):
             num_heads=self.num_heads,
             dropout=self.dropout
         )
+
         self.inputs = tf.random.uniform((self.batch_size, self.seq_len), maxval=self.vocab_size, dtype=tf.int32)
         self.padding_mask = tf.random.uniform((self.batch_size, 1, 1, self.seq_len))
 
@@ -39,21 +40,17 @@ class TestEncoder(unittest.TestCase):
         try:
             _ = self.encoder_model([self.inputs, self.padding_mask])
         except Exception as e:
-            self.fail(f"Encoder failed connectivity test with error: {e}")
+            self.fail(f"Encoder model failed connectivity test with error: {e}")
 
-    def test_embedding_application(self):
-        embedding_layer = self.encoder_model.get_layer(index=2)
+    def test_embedding_and_positional_encoding(self):
+
+        # Check embedding layer
+        embedding_layer = self.encoder_model.layers[1]
         self.assertIsInstance(embedding_layer, tf.keras.layers.Embedding)
-        self.assertEqual(embedding_layer.input_dim, self.vocab_size)
-        self.assertEqual(embedding_layer.output_dim, self.d_model)
 
-    def test_positional_encoding_application(self):
-        positional_encoding_layer = self.encoder_model.get_layer(index=4)
+        # Check positional encoding layer
+        positional_encoding_layer = self.encoder_model.layers[3]
         self.assertIsInstance(positional_encoding_layer, PositionalEncoding)
-
-    def test_number_of_encoder_layers(self):
-        encoder_layers = [layer for layer in self.encoder_model.layers if 'encoder_layer_' in layer.name]
-        self.assertEqual(len(encoder_layers), self.num_layers)
 
 if __name__ == '__main__':
     unittest.main()
