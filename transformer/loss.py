ec46c1eca1d3c7430874ce59dffaa6a0633d091f
namy2 <namy2@mcmaster.ca>
2024-05-25 14:04:33 +0900

    added loss function and custom scheduler"

---
+++ transformer/loss.py
@@ -0,0 +1,18 @@
+"""
+Loss function for transformer model
+"""
+
+import tensorflow as tf
+
+from transformer.global_var import MAX_LENGTH
+
+def loss_function(y_true, y_pred):
+    y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')(y_true, y_pred)
+
+    mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+    loss = tf.multiply(loss, mask)
+
+    return tf.reduce_mean(loss)
---
+++ transformer/training/custom_schedule.py
@@ -0,0 +1,28 @@
+"""
+Custom Schedule for learning rate
+"""
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+
+    def __init__(self, d_model, warmup_steps=4000):
+        super(CustomSchedule, self).__init__()
+        self.d_model = d_model
+        self.d_model = tf.cast(self.d_model, tf.float32)
+        self.warmup_steps = warmup_steps
+
+    def __call__(self, step):
+        arg1 = tf.math.rsqrt(step)  # 1 / sqrt(step)
+        arg2 = step * (self.warmup_steps**-1.5) # step / (warmup_steps ^ 1.5)
+
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+    
+if __name__ == '__main__':
+    sample_learning_rate = CustomSchedule(d_model=128)
+
+    plt.plot(sample_learning_rate(tf.range(200000, dtype=tf.float32)))
+    plt.ylabel("Learning Rate")
+    plt.xlabel("Train Step")
+    plt.show()
